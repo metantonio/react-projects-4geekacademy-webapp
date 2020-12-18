fa6ce8537511cbab31320ef1ce84c582cdcfd47d
Antonio Martínez <antonio_martinez88@hotmail.com>
2020-12-18 16:17:07 -0400

    agregando favoritos

---
+++ src/js/component/favorite.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Dropdown } from "semantic-ui-react";
+
+const options = [{ key: "delete", icon: "delete", text: "Test", value: "delete" }];
+
+const Favorite = () => {
+	let newDiv = "Favorite 🤍";
+	return newDiv;
+};
+
+export default Favorite;
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Favorite from "./favorite.js";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				{/* <image
+					src="https://www.mujer.com.pa/sites/default/files/field/image/star-wars-revista-mujer-.jpg"
+					id="logo-home"
+				/> */}
+				{<span className="navbar-brand mb-0 h1 col-8">Star Wars</span>}
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div className="ml-auto col-4">
+				<button className="btn btn-primary col-4">{Favorite()}</button>
 			</div>
 		</nav>
 	);
